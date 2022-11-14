8c9cf590e57bfd3c6f1fb9c16aa7783a621a95a2
ilyaDavidov <ilya.davidov63@mail.ru>
2022-11-14 17:13:16 +0200

    d

---
+++ MauiApp12/MainPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿namespace MauiApp12;
+
+public partial class MainPage : ContentPage
+{
+	public int operCost =0;
+	public int lorCost=0;
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+	public void SumCost(int s, int u)
+	{
+		Cost.Text = "  : " + (s + u).ToString();
+	}
+	private void RadioButton_CheckedChanged(object sender, CheckedChangedEventArgs e)
+	{
+		operCost = int.Parse((sender as RadioButton).Value.ToString());
+		SumCost(operCost, lorCost);
+	}
+	private void RadioButton_CheckedChanged_1(object sender, CheckedChangedEventArgs e)
+	{
+        lorCost = int.Parse((sender as RadioButton).Value.ToString());
+		SumCost(operCost, lorCost);
+	}
+
+
+}
+
